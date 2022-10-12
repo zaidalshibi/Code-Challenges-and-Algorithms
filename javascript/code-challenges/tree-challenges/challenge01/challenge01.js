4c6512f53782e7d858744f1038f2fe1c68d7a223
Zaid alshibi <zaidealshibi@gmail.com>
2022-10-12 22:59:02 +0300

    solved challenge01 of the Binary tree

--- javascript/code-challenges/tree-challenges/challenge01/challenge01.js
+++ javascript/code-challenges/tree-challenges/challenge01/challenge01.js
@@ -1,1 +1,21 @@
-// Write here the code challenge solution
+'use strict';
+
+function Node ( value, left = null, right = null ) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+}
+
+function Tree ( preOrder, inOrder ) {
+    if ( inOrder.length === 0 ) return null;
+    if ( preOrder.length === 0 ) return null;
+    const root = preOrder[ 0 ];
+    let bT = new Node( root );
+    let rootIndex = inOrder.indexOf( root );
+    bT.left = Tree( preOrder.slice( 1, rootIndex + 1 ), inOrder.slice( 0, rootIndex ) );
+    bT.right = Tree( preOrder.slice( rootIndex + 1 ), inOrder.slice( rootIndex + 1 ) );
+    return bT;
+};
+
+
+module.exports = { Node, Tree };
