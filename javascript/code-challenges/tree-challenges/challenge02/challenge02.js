7f14f02795420020997559474532e487db7ab516
Zaid alshibi <zaidealshibi@gmail.com>
2022-10-12 23:51:53 +0300

    solved challenge02 of the Binary tree

--- javascript/code-challenges/tree-challenges/challenge02/challenge02.js
+++ javascript/code-challenges/tree-challenges/challenge02/challenge02.js
@@ -1,1 +1,11 @@
-// Write here the code challenge solution
+function breadthFirstCheck ( p, q ) {
+    if ( !p && !q ) return true;
+    if ( !p || !q ) return false;
+    if ( p.value !== q.value ) return false;
+    return (
+        breadthFirstCheck( p.left, q.left ) &&
+        breadthFirstCheck( p.right, q.right )
+    );
+}
+
+module.exports = breadthFirstCheck;
